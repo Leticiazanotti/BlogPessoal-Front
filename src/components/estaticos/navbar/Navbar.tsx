0f605cdd3ddb9838e8906fa52c5c80622f97e170
Leticiazanotti <leticia.zanotti2003@gmail.com>
2022-11-03 23:58:59 -0300

    react router

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 import {Box} from '@mui/material';
 
 function Navbar() {
@@ -14,31 +16,34 @@ function Navbar() {
             </Box>
 
             <Box display="flex" justifyContent="start">
-                <Box mx={1} style={{ cursor: "pointer" }}>
+                <Box mx={1} className='cursor'>
                     <Typography variant="h6" color="inherit">
-                        home
+                        Home
                     </Typography>
                 </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
+                <Box mx={1} className='cursor'>
                     <Typography variant="h6" color="inherit">
-                        postagens
+                        Postagens
                     </Typography>
                 </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
+                <Box mx={1} className='cursor'>
                     <Typography variant="h6" color="inherit">
-                        temas
+                        Temas
                     </Typography>
                 </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
+                <Box mx={1} className='cursor'>
                     <Typography variant="h6" color="inherit">
-                        cadastrar tema
+                        Cadastrar tema
                     </Typography>
                 </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
+                <Link to='/login' className='text-decorator-none'>
+                <Box mx={1} className='cursor'>
                     <Typography variant="h6" color="inherit">
-                        logout
+                        Logout
                     </Typography>
                 </Box>
+                </Link> 
+                
             </Box>
 
         </Toolbar>
